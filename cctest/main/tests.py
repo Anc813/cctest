6365189a3e2dad67d54bf8302512998dfede4b48
anc813 <anc813@gmail.com>
2013-11-29 23:28:48 +0200

    ticket:1 add test, html files, css, url

--- cctest/main/tests.py
+++ cctest/main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import People
+from .views import HomeView
 
+from django.core.urlresolvers import reverse
 # Create your tests here.
+
+class HomeViewTests(TestCase):
+
+    def test_home_view(self):
+        response = self.client.get(reverse('main:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "42 Coffee Cups Test Assignment.")
