8cdd437ad082540e28ba73bf994de501ad3eedb8
anc813 <anc813@gmail.com>
2013-11-30 15:25:48 +0200

    ticket:3 implementation "Create middleware that stores all http requests in the DB"

--- cctest/main/models.py
+++ cctest/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class People(models.Model):
@@ -14,3 +15,25 @@ class People(models.Model):
 
     def __unicode__(self):
         return "%s %s" % (self.surname, self.name)
+
+class HTTPRequest(models.Model):
+    # https://docs.djangoproject.com/en/1.6/ref/request-response/#django.http.HttpRequest
+    body = models.TextField(null=True)
+    path = models.CharField(max_length=8192, null=True)
+    path_info = models.CharField(max_length=8192, null=True)
+    method = models.CharField(max_length=4)
+    encoding = models.CharField(max_length=32, null=True)
+    GET = models.TextField(null=True)
+    POST = models.TextField(null=True)
+    COOKIES = models.TextField(null=True)
+    FILES = models.TextField(null=True)
+    META = models.TextField(null=True)
+    user = models.ForeignKey(User, null=True)
+    session = models.TextField(null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '%s %s %s' % (self.timestamp, self.method, self.path)
+
+    class Meta:
+        ordering = ['-timestamp']
--- cctest/main/views.py
+++ cctest/main/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import People
+from django.views.generic import DetailView, ListView
+from .models import People, HTTPRequest
 # Create your views here.
 
 class HomeView(DetailView):
     model = People
     template_name = "main/home.html"
+
+class HTTPRequestView(ListView):
+    model = HTTPRequest
+    template_name = "main/requests.html"
+
+    def get_queryset(self):
+        return HTTPRequest.objects.all()[:10]
