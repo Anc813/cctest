e67cec46e97e53abff300887acfc17e1a3985ca1
anc813 <anc813@gmail.com>
2013-11-30 15:24:43 +0200

    ticket:3 test only and no implementation (tests fail)

--- cctest/main/tests.py
+++ cctest/main/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
-from .models import People
-from .views import HomeView
+from .models import People, HTTPRequest
+from .views import HomeView, HTTPRequestView
+from .middleware import StoreRequestsDB
 
 from django.core.urlresolvers import reverse
-# Create your tests here.
 
 class HomeViewTests(TestCase):
 
@@ -11,3 +11,18 @@ class HomeViewTests(TestCase):
         response = self.client.get(reverse('main:home'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "42 Coffee Cups Test Assignment.")
+
+class HTTPRequestViewTests(TestCase):
+
+    def test_HTTPRequestView(self):
+        response = self.client.get(reverse('main:requests-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "First 10 http requests that are stored by middleware.")
+
+class StoreRequestsDBMiddlewareTests(TestCase):
+
+    def test_HTTPRequest_model_increased(self):
+        count = HTTPRequest.objects.all().count()
+        response = self.client.get(reverse('main:requests-list'))
+        new_count = HTTPRequest.objects.all().count()
+        self.assertEqual(count + 1, new_count)
