4c14e88883d2b2b09e2f037c310eee86fde2ac99
anc813 <anc813@gmail.com>
2013-12-01 13:40:32 +0200

    ticket:5 add tests Create page with a form that allows to edit data, presented on the main page

--- cctest/main/tests.py
+++ cctest/main/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from .models import People, HTTPRequest
-from .views import HomeView, HTTPRequestView
+from .views import HomeView, HTTPRequestView, HomeEditView
 from .middleware import StoreRequestsDB
 
 from django.core.urlresolvers import reverse
@@ -37,3 +37,50 @@ class SettingsContextProcessorTests(TestCase):
         c = RequestContext(response)
         self.assertTrue('settings' in c)
         self.assertEquals(c['settings'], django_settings)
+
+class PeopleMethodTests(TestCase):
+
+    def test_get_absolute_url(self):
+        """
+        contains at least one element, loaded from fixtures
+        get_absolute_url is accesible
+        """
+        item = People.objects.all()[:1]
+        self.assertTrue(item)
+        response = self.client.get(item[0].get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "42 Coffee Cups Test Assignment.")
+
+class HomeEditViewTests(TestCase):
+
+    def test_unathorized_access_is_forbidden(self):
+        """
+        editing is forbidden withouth login
+        """
+        item = People.objects.all()[:1][0]
+        response = self.client.get(reverse("main:edit", args=[item.pk]))
+        self.assertEqual(response.status_code, 302)
+
+    def test_authorized_access_is_granted(self):
+        '''
+        editing with admin/admin is granted
+        '''
+        item = People.objects.all()[:1][0]
+        self.client.login(username='admin', password='admin')
+        response = self.client.get(reverse("main:edit", args=[item.pk]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_form_is_failed(self):
+        item = People.objects.all()[:1][0]
+        self.client.login(username='admin', password='admin')
+        response = self.client.post(reverse("main:edit", args=[item.pk]), {'name':'name'})
+        self.assertFormError(response, 'form', 'birth_date', 'This field is required.')
+
+    def test_form_is_success(self):
+        item = People.objects.all()[:1][0]
+        self.client.login(username='admin', password='admin')
+        response = self.client.post(reverse("main:edit", args=[item.pk]),
+                                    {'name':'1', 'surname':'1', 'birth_date':'2012-11-11',
+                                     'bio':'1', 'email' :'admin@example.com', 'other_contacts' : '1'})
+        #print response.context['form'].errors
+        self.assertEqual(response.status_code, 302)
