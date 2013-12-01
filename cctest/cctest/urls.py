aa0fefa1b0f1d1ec8c60771bc9692b8112dfe86c
anc813 <anc813@gmail.com>
2013-12-01 13:41:54 +0200

    ticket:5 add authorisation, edit page Create page with a form that allows to edit data, presented on the main page

--- cctest/cctest/urls.py
+++ cctest/cctest/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,6 +13,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     (r'^', include('main.urls', namespace = "main")),
     url(r'^admin/', include(admin.site.urls)),
-)
-
-
+    url(r'^accounts/login/$', login, name="login"),
+    url(r'^accounts/logout/$', logout, name="logout", kwargs={"next_page":"main:home"}),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
