be90c96c76937417eae4b2f08d1c5329a15f106e
anc813 <anc813@gmail.com>
2013-12-03 15:22:45 +0200

    ticket 9: add implementation, duplicate output to STDERR with prefix "error: " Create django command that prints all project models and the count of objects in every model

---
+++ cctest/main/management/commands/models_info.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import get_models
+from django.template.defaultfilters import pluralize
+
+class Command(BaseCommand):
+    help = 'prints all project models and the count of objects in every model'
+
+    def handle(self, *args, **options):
+        models = get_models()
+
+        if len(models) == 0:
+            s = "no models found"
+            self.stdout.write(s)
+            self.stderr.write(s)
+
+        for model in models:
+            count = model.objects.all().count()
+            s = "model %s.%s has %d object%s" % (model._meta.app_label, model._meta.model_name, count, pluralize(count))
+            self.stdout.write(s)
+            self.stderr.write('error: %s' % s)
